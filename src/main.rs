6d1e2ca42bb20932876e9b4fd9339c32ff4140d4
Tony hart <tonyhart2020@gmail.com>
2022-11-23 00:05:54 +0700

    deploy railway

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use axum::{extract::Query, response::Html, routing::get, Router};
+use rand::{thread_rng, Rng};
+use serde::Deserialize;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// `Deserialize` need be implemented to use with `Query` extractor.
+#[derive(Deserialize)]
+struct RangeParameters {
+    start: usize,
+    end: usize,
+}
+
+async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
+    // Generate a random number in range parsed from query.
+    let random_number = thread_rng().gen_range(range.start..range.end);
+
+    // Send response in html format.
+    Html(format!("<h1>Random Number: {}</h1>", random_number))
+}
